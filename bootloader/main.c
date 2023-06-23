943da2cabd12b0e643b1973da9f972de98d41af0
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-06-23 00:29:08 -0300

    Update main.c

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,7 +1,30 @@
-#include <stdint>
+#include <stdint.h>
 
 typedef void (*KernelMain)(void);
 
+// BPP is BYTES PER PIXEL
+
+#define FB_Base 0x100000
+#define Width 800
+#define Height 575
+#define BPP 4
+
+void DrawBG(void) {
+    uint32_t* FrameBuffer = (uint32_t*)FB_BASE;
+
+    for (int Y = 0; Y < Height; Y++) {
+        for (int X = 0; X < Width; X++) {
+            int Index = Y * Width + X;
+
+            FrameBuffer[Index] = 0xFF0000FF;
+        }
+    }
+}
+
 void BootMain(void) {
+    DrawBG();
     
+    KernelMain Kernel = (KernelMain)0x8000;
+
+    Kernel();
 }
